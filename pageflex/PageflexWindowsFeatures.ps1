eb8ddee22fb3a4d2d9e3b3e4789324dd93886e35
AzureAD\StevenOBrien <stevenobrien@devia.com>
2017-11-10 12:31:11 -0500

    Initial Version

---
+++ pageflex/PageflexWindowsFeatures.ps1
@@ -0,0 +1,23 @@
+﻿<#Copyright 2017 Devia Software, LLC www.devia.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Description:
+This Powershell Script installs All necessary Features required for PF SErver and Storefront to run.
+
+#>
+
+Install-WindowsFeature Web-Server,Web-Server,Web-Common-Http,Web-Default-Doc,Web-Dir-Browsing,Web-Http-Errors,Web-Static-Content,Web-Http-Redirect,Web-Health,Web-Http-Logging
+Install-WindowsFeature NET-Framework-Features,NET-Framework-Core
+Install-WindowsFeature MSMQ
+Install-WindowsFeature Web-Performance, Web-Stat-Compression, Web-Dyn-Compression
+Install-WindowsFeature Web-Security,Web-Filtering, Web-Windows-Auth
+Install-WindowsFeature Web-App-Dev,Web-Net-Ext,Web-Net-Ext45,Web-Asp-Net,Web-Asp-Net45,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-Includes
+Install-WindowsFeature Web-Mgmt-Tools,Web-Mgmt-Console,Web-Mgmt-Compat,Web-Metabase,Web-Lgcy-Mgmt-Console,Web-Lgcy-Scripting,Web-WMI,Web-Scripting-Tools
+
+
+
